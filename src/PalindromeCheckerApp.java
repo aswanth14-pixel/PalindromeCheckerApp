a1977afce387bfc315fc37b75298f5603b0374c2
aswanth14-pixel <aswanthnarayan2615@gmail.com>
2026-02-20 22:54:53 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,10 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        // ===== UC1: Application Entry =====
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
 }
