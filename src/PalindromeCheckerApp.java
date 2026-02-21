f8ec086e0a1931a67c4577c58600aed8d0fb08fd
aswanth14-pixel <aswanthnarayan2615@gmail.com>
2026-02-21 06:45:03 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,5 +6,19 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully.");
+
+        // ===== UC2 =====
+        String input1 = "madam";
+        boolean isPalindrome1 = true;
+
+        for (int i = 0; i < input1.length() / 2; i++) {
+            if (input1.charAt(i) != input1.charAt(input1.length() - 1 - i)) {
+                isPalindrome1 = false;
+                break;
+            }
+        }
+
+        System.out.println("UC2 Result : " + isPalindrome1);
+        System.out.println();
     }
 }
