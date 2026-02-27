19dbc86ff8fb4235ebc4bb9857675d2a8d83a2d2
aswanth14-pixel <aswanthnarayan2615@gmail.com>
2026-02-27 22:07:03 +0530

    Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,71 +1,29 @@
 public class PalindromeCheckerApp {
 
-    // Node class for singly linked list
-    static class ListNode {
-        char data;
-        ListNode next;
-
-        ListNode(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
     public static void main(String[] args) {
 
-        String input = "level";
+        String input = "madam";
 
-        // Step 1: Convert string to linked list
-        ListNode head = null;
-        ListNode tail = null;
-
-        for (char c : input.toCharArray()) {
-            ListNode newNode = new ListNode(c);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        // Step 2: Find middle using fast & slow pointer
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Step 3: Reverse second half
-        ListNode prev = null;
-        ListNode current = slow;
-
-        while (current != null) {
-            ListNode nextTemp = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextTemp;
-        }
-
-        // Step 4: Compare halves
-        ListNode firstHalf = head;
-        ListNode secondHalf = prev;
-
-        boolean isPalindrome = true;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                isPalindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
+        boolean isPalindrome = isPalindromeRecursive(input, 0, input.length() - 1);
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    // Recursive method
+    private static boolean isPalindromeRecursive(String str, int start, int end) {
+
+        // Base Case 1: If pointers cross or meet
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don't match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call shrinking the problem
+        return isPalindromeRecursive(str, start + 1, end - 1);
     }
 }
